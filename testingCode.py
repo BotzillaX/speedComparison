8edfce67c99f248dfdae8f62548782320106a726
KevinFritsch <kevinfritsch7@yahoo.de>
2024-05-08 22:03:46 +0200

    added writing as a task and still it seems to be much faster

--- testingCode.py
+++ testingCode.py
@@ -1,4 +1,6 @@
 from speedComparison import getTime
+from hashMap import key_constants
+import time
 import random
 import pyautogui as py
 py.FAILSAFE = False
@@ -8,17 +10,29 @@ user32 = ctypes.WinDLL('user32', use_last_error=True)
 
 
 
-# def move_mouse_ctypes(x, y): #10.000 times faster
-#     user32.SetCursorPos(x, y)
 
-# def move_mouse_pyauto(x, y):
-#     py.moveTo(x, y)
-
-
+def keyComparison():
+    KEYEVENTF_KEYDOWN = 0x0
+    KEYEVENTF_KEYUP = 0x2
+    text = """hello my name is"""
 
 
+    for char in text:
+        vk_code = key_constants[char]
+        user32.keybd_event(vk_code, 0, KEYEVENTF_KEYDOWN, 0)
+        
+        user32.keybd_event(vk_code, 0, KEYEVENTF_KEYUP, 0)
 
 
 
 
-getTime()
+def keyPyautogui():
+    py.typewrite("""hello my name is""")
+
+
+# keyComparison(text)
+
+getTime(keyComparison, 1)
+getTime(keyPyautogui, 1)
+
+hello my name ishello my name is
