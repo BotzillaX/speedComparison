800caa7c555d3bcab84e86d99c2f7556eb8ea238
KevinFritsch <kevinfritsch7@yahoo.de>
2024-05-09 17:33:44 +0200

    using pyautogui for locating is 50% faster.

--- testingCode.py
+++ testingCode.py
@@ -7,35 +7,13 @@ py.FAILSAFE = False
 import ctypes
 user32 = ctypes.WinDLL('user32', use_last_error=True)
 import keyboard
+import pyautogui
+import sys
 
 
 
 
-
-
-
-def keyComparison():
-    KEYEVENTF_KEYDOWN = 0x0
-    KEYEVENTF_KEYUP = 0x2
-    text = """hello my name is"""
-
-
-    for char in text:
-        vk_code = key_constants[char]
-        user32.keybd_event(vk_code, 0, KEYEVENTF_KEYDOWN, 0)
-        
-        user32.keybd_event(vk_code, 0, KEYEVENTF_KEYUP, 0)
-
-
-
-
-def keyPyautogui():
-    keyboard.write("""hello my name is""")
-
-
-# keyComparison(text)
-
-getTime(keyComparison, 30)
-getTime(keyPyautogui, 30)
+#getTime(oldLogic, 1)
+#getTime(newLogic, 1)
 
 
